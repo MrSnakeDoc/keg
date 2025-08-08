54bca8dc66f3164dabaf827206c1eba8c12883be
MrSnakeDoc <dev@nexacloud.dev>
2025-08-08 15:29:48 +0000

    refactor(cli): add generic middleware factory and context system with typed injection
    - Replaced hardcoded config loading with middleware-based approach
    - Added RequireConfig, loadPkgList, and HomebrewCheck middlewares
    - Introduced context keys and generic Get[T] helper to retrieve typed data from cmd.Context()
    - Updated all commands to use the new context system and middleware factory

--- internal/middleware/middleware.go
+++ internal/middleware/middleware.go
@@ -19,38 +19,6 @@ type MiddlewareChain func(factory CommandFactory) CommandFactory
 
 type contextKey string
 
-// func UseMiddlewareChain(middlewares ...MiddlewareFunc) MiddlewareChain {
-// 	return func(factory CommandFactory) CommandFactory {
-// 		return func() *cobra.Command {
-// 			cmd := factory()
-
-// 			original := cmd.PreRunE
-
-// 			cmd.PreRunE = func(c *cobra.Command, args []string) error {
-// 				var chain func(cmd *cobra.Command, args []string) error
-// 				chain = func(cmd *cobra.Command, args []string) error {
-// 					if original != nil {
-// 						return original(cmd, args)
-// 					}
-// 					return nil
-// 				}
-
-// 				for i := len(middlewares) - 1; i >= 0; i-- {
-// 					mw := middlewares[i]
-// 					next := chain
-// 					chain = func(cmd *cobra.Command, args []string) error {
-// 						return mw(cmd, args, next)
-// 					}
-// 				}
-
-// 				return chain(c, args)
-// 			}
-
-// 			return cmd
-// 		}
-// 	}
-// }
-
 func UseMiddlewareChain(middlewares ...MiddlewareFunc) MiddlewareChain {
 	return func(factory CommandFactory) CommandFactory {
 		return func() *cobra.Command {
